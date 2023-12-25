4ac1b9e57fd5c35c8ac13818e8026cf8bcbf92a6
Rabbani <73340081+ka3ax01@users.noreply.github.com>
2023-12-26 03:08:09 +0600

    home page done

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,11 @@
+import 'dart:io';
+
 import 'package:finstagram/pages/feed_page.dart';
 import 'package:finstagram/pages/profile_page.dart';
 import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:finstagram/services/firebase_service.dart';
+import 'package:get_it/get_it.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,11 +17,20 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
+  FirebaseService? _firebaseService;
+
   int _currentIndex = 0;
   final List<Widget> _listOfWidgets = [
     FeedPage(),
     ProfilePage(),
   ];
+
+  @override
+  void initState() {
+    super.initState();
+    _firebaseService = GetIt.instance.get<FirebaseService>();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,13 +38,20 @@ class HomePageState extends State<HomePage> {
         title: const Text("Finstagram"),
         backgroundColor: Colors.black,
         actions: [
-          GestureDetector(child: const Icon(Icons.add_a_photo)),
+          GestureDetector(
+            onTap: _postImage,
+            child: const Icon(Icons.add_a_photo),
+          ),
           Padding(
             padding: const EdgeInsets.only(
               left: 8.0,
               right: 8.0,
             ),
             child: GestureDetector(
+              onTap: () async {
+                await _firebaseService!.logout();
+                Navigator.popAndPushNamed(context, '/login');
+              },
               child: const Icon(Icons.logout),
             ),
           ),
@@ -50,11 +71,22 @@ class HomePageState extends State<HomePage> {
           });
         },
         items: const [
-          BottomNavigationBarItem(label: "feed", icon: Icon(Icons.feed)),
+          BottomNavigationBarItem(
+            label: "feed",
+            icon: Icon(Icons.feed),
+          ),
           BottomNavigationBarItem(
             label: "profile",
             icon: Icon(Icons.account_box),
           )
         ]);
+  }
+
+  void _postImage() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      type: FileType.image,
+    );
+    File image = File(result!.files.first.path!);
+    await _firebaseService!.postImage(image);
   }
 }
